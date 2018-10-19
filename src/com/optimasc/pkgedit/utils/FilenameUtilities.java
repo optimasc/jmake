37691c76f3cda6c3628e9388fde54c0d6d5d2442
Carl Eric Codere <42296635+ccodere@users.noreply.github.com>
2018-10-20 00:09:10 +0200

    * removeSuffix should keep complete path specification.

--- src/com/optimasc/pkgedit/utils/FilenameUtilities.java
+++ src/com/optimasc/pkgedit/utils/FilenameUtilities.java
@@ -2,8 +2,26 @@ package com.optimasc.pkgedit.utils;
 
 public class FilenameUtilities
 {
-
+  
   public static String removeSuffix(String s)
+  {
+
+    String filename;
+
+    // Remove the path up to the filename.
+    int lastSeparatorIndex = s.lastIndexOf(".");
+    if (lastSeparatorIndex == -1)
+    {
+      filename = s;
+    } else
+    {
+      filename = s.substring(0,lastSeparatorIndex);
+    }
+    return filename;
+  }
+  
+  
+  public static String extractFilename(String s)
   {
 
     String separator = "/";
